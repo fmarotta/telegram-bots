e25a91fecbcf1ec4ea047fde8c67bbe615ac228b
Federico Marotta <federico.marotta96@gmail.com>
2017-03-04 01:06:07 +0100

    Fix

--- scripts/mail.py
+++ scripts/mail.py
@@ -5,45 +5,42 @@ and sends the plain text parts to a Telegram user through a bot.
 
 """
 
-# I am so lazy that I hard coded a bunch of things... sorry
-
 import telepot
 import ssl
 import imaplib
 import email
+import sys
 import time
 from email.header import decode_header
 from bs4 import BeautifulSoup
 
+if len(sys.argv) != 3:
+    print('Usage: python mail.py <bot\'s token> <your Telegram ID>')
+    sys.exit()
+
 # Initialize the bot
-# TODO: read my id and the bot token from a file,
-# or from the command line arguments
-bot = telepot.Bot(#INSERT TOKEN#)
-my_id = #INSERT ID#
+token = sys.argv[1]
+my_id = sys.argv[2]
+bot = telepot.Bot(token)
 
 # Define the mail account class
 class ImapAccount:
-    host = ''
-    port = 993
-    username = ''
-    password = ''
-    folders = ['inbox']
+    def __init__(self, host, port, username, password, folders):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.folders = folders
 
-# Define the accounts as an array of objects
-# TODO: read the account info from a file for easier modification
-# or better yet let the bot ask the user
-federicomarotta96ATgmailDOTcom = ImapAccount()
-federicomarotta96ATgmailDOTcom.host = #INSERT MAIL SERVER#
-federicomarotta96ATgmailDOTcom.username = #INSERT USERNAME#
-federicomarotta96ATgmailDOTcom.password = #INSERT PASSWORD#
+# Define the accounts
+# Feature: let the bot ask the user for new accounts
+accounts = []
 
-accounts = [federicomarotta96ATgmailDOTcom]
+accounts.append(ImapAccount('<yourmailserver', 993, '<yourusername>', '<yourpassword>', ['inbox']))
+# By using this format you can append how many accounts as you want.
 
-# The function which fetches the messages
+# The function which fetch the messages
 def fetch_email(account):
-    # Variables definition
-    payload = ''
-    attachments = ''
     msg = []
     # Create a ssl context for the connection to the imap server
     context = ssl.create_default_context()
@@ -75,6 +72,9 @@ def fetch_email(account):
 
         uid_list = uids[0].split()
         for uid in uid_list:
+            # Variables definition
+            payload = ''
+            attachments = ''
             # Fetch the unread emails
             result, data = mail.uid('fetch', uid, '(RFC822)') # By fetching the whole RFC 822 message, it becomes automatically read
             if result != 'OK':
@@ -109,8 +109,8 @@ def fetch_email(account):
                 pass
 
             # Decode and save only the plain text body
-            # NOTE: we do not use the method get_body(); we use
-            # walk() instead, because we think it is safer
+            # NOTE: we do not use the method get_body; we use
+            # walk instead, because we think it is safer
             if not message.is_multipart():
                 payload = message.get_payload(decode=True).decode('utf-8')
             else:
@@ -129,13 +129,13 @@ def fetch_email(account):
             # Parse the body for any eventual HTML and return only the text
             soup = BeautifulSoup(payload, 'html5lib')
             payload = soup.get_text()
-                
+
             # Instead of letting this field blank, tell 'None'
             if attachments == '':
                 attachments = 'None'
 
             # Messages to send if there are emails
-            msg.insert(0, '*You\'ve got a new e-mail!*\n```\nFrom: {}\nTo: {}\nSubject: {}\nAttachments: {}```\n\n{}'.format(sender, account.username, subject, attachments, payload))
+            msg.insert(0, '<b>You\'ve got a new e-mail!</b>\n<pre>From: {}\nTo: {}\nSubject: {}\nAttachments: {}\n</pre>\n{}'.format(sender, account.username, subject, attachments, payload))
 
     # Close the connection and logout
     mail.close()
@@ -148,9 +148,8 @@ while 1:
         msg = fetch_email(account)
         for mex in msg:
             try:
-                bot.sendMessage(my_id, mex, 'Markdown')
-            except telepot.exception.TelegramError:
-                bot.sendMessage(my_id, 'You\'ve got new mails to {}, but I could\'t send them because of a Telegram error'.format(account.username))
+                bot.sendMessage(my_id, mex, 'HTML')
+            except telepot.exception.TelegramError as err:
+                bot.sendMessage(my_id, 'You\'ve got new mails to {}, but I could\'t send them because of a Telegram error {}'.format(account.username, err))
     # Execute this every 14 minutes
     time.sleep(14 * 60)
-
